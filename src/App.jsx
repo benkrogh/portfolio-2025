00af6aa2d6519868978c9fb1c6985f1135b67d33
Benjamin Krogh <benkrogh@gmail.com>
2024-11-29 10:57:21 -0800

    File structure, naming
    Changed the file structure, updated the naming to .jsx syntax, and updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import BlogIndex from './components/BlogIndex'
-import DesignPrinciples from './components/DesignPrinciples'
-import MusicPlayer from './components/MusicPlayer'
+import React from 'react';
+import BlogIndex from './components/BlogIndex';
+import DesignPrinciples from './components/DesignPrinciples';
+import MusicPlayer from './components/MusicPlayer';
 
 function App() {
   return (
