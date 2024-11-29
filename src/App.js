6d6eac468c2c24309efd2226e28da0308a0c4597
Benjamin Krogh <benkrogh@gmail.com>
2024-11-29 10:30:53 -0800

    Structure cleanup, naming
    Changed naming to proper syntax, added src folder, added App.js and main.js files.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import BlogIndex from './components/BlogIndex'
+import DesignPrinciples from './components/DesignPrinciples'
+import MusicPlayer from './components/MusicPlayer'
+
+function App() {
+  return (
+    <div>
+      <BlogIndex />
+      <DesignPrinciples />
+      <MusicPlayer />
+    </div>
+  )
+}
+
+export default App
