a7466404e7bd5b2c2a0178f1eaee1b24b51683f4
Benjamin Krogh <benkrogh@gmail.com>
2024-11-29 11:11:12 -0800

    Removed the music player import
    Troubleshooting the build, isolating Blog and design principles seems to be working.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import BlogIndex from './components/BlogIndex';
-import DesignPrinciples from './components/DesignPrinciples';
-import MusicPlayer from './components/MusicPlayer';
-
+import BlogIndex from './components/BlogIndex.jsx';
+import DesignPrinciples from './components/DesignPrinciples.tsx';
 function App() {
   return (
     <div>
       <BlogIndex />
       <DesignPrinciples />
-      <MusicPlayer />
     </div>
   )
 }
