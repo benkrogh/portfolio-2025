088fd85b31acbfcab789d5dca26217e1a41ba183
Nathan Searles <nsearles@gmail.com>
2024-11-30 15:45:28 -0800

    File format updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 // astro.config.mjs
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [
     react(), // Enable React components
-    tailwind() // Enable Tailwind CSS
+    tailwind(), // Enable Tailwind CSS
   ],
   // Enable building of dynamic routes
   build: {
-    format: 'directory'
-  }
+    format: "directory",
+  },
 });
